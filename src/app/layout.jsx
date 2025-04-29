0ef7b69bfeb2431f5675428762e264388043ffef
Mr-S41to <matheussaito95k@gmail.com>
2025-04-29 15:51:36 -0300

    Correção de icone.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,13 +3,15 @@ import "./globals.css";
 export const metadata = {
   title: "Matheus Saito",
   description: "Portfólio de Desenvolvedor",
+  icons: {
+    icon: '/favicon.ico',
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <head>
-        <link rel="icon" href="/favicon.ico" />
         <meta
           name="google-site-verification"
           content="52NdwcnzmnAzuho2LZZi8yUK4E01KCtjf7kKTh4qU30"
